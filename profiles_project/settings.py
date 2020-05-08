f0093ec49bae614b91585ba5dbccc893e5d3ac81
Andrey Tokarev <abtokarev@gmail.com>
2020-05-08 17:40:33 +0300

    Revert to sqlite

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -76,16 +76,26 @@ WSGI_APPLICATION = 'profiles_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'profileapi',
+#         'USER': 'postgresadm',
+#         'PASSWORD': 'Alcatel4400',
+#         'HOST': 'db',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'profileapi',
-        'USER': 'postgresadm',
-        'PASSWORD': 'Alcatel4400',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+
 
 
 # Password validation
